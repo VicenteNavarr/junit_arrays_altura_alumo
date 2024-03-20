11ac46f17d5575d310a994a16dc9ae54335aff7c
administrador <vicentenavarr@gmail.com>
2024-03-20 12:45:26 +0100

    Actualización 20_Marzo

--- src/test/java/org/iesvdm/AlturaAlumnoTest.java
+++ src/test/java/org/iesvdm/AlturaAlumnoTest.java
@@ -102,6 +102,7 @@ public class AlturaAlumnoTest {
 
         //do
         double [] arrayActual = AlturaAlumno.aniadeAltura(array);
+
         double [] arrayEsperado = Arrays.copyOf(array,array.length+1);
         arrayEsperado[arrayEsperado.length-1]=alturaPorDefecto;
 
@@ -113,19 +114,183 @@ public class AlturaAlumnoTest {
     }
 
     @Test
-    void buscaNombreTest1() {
+    void buscaNombreTest1encuentra() {
 
         //when
-        int posicionEncontrada = -1;
-        int index = 0;
+        String [] array = {"uno", "dos", "tres"};
+        String nombre= "uno";
+
+        //do - then
+        assertEquals(0, AlturaAlumno.buscaNombre(array, nombre));
+
+    }
+
+    @Test
+    void buscaNombreTest2NoEncuentra() {
+
+        //when
+        String [] array = {"uno", "dos", "tres"};
+        String nombre= "cuatro";
+
+        //do - then
+        assertEquals(-1, AlturaAlumno.buscaNombre(array, nombre));
+
+    }
+
+    @Test
+    void buscaNombreTest3ArrayVacio() {
+
+        //when
+        String [] array = {};
+        String nombre= "cuatro";
+
+        //do - then
+        assertEquals(-1, AlturaAlumno.buscaNombre(array, nombre));
+
+    }
+
+    @Test
+    void buscaNombreTest4NombreVacio() {
+
+        //when
+        String [] array = {"uno", "dos", "tres"};
+        String nombre= "";
+        String nombre2= null;
+
+        //do - then
+        assertEquals(-1, AlturaAlumno.buscaNombre(array, nombre));
+        assertEquals(-1, AlturaAlumno.buscaNombre(array, nombre2));
+
+    }
+
+
+    @Test
+    void mostrarTest1Ok() {
+
+        //when
+        String [] arrayNombre = {"uno", "dos", "tres"};
+        double[] arrayAltura = {1.6, 1.8, 1.7};
+
+        //do - then
+        assertDoesNotThrow(() -> {AlturaAlumno.mostrar(arrayNombre, arrayAltura);});
+
+    }
+
+    @Test
+    void mostrarTest2NotOk() {
+
+        //when
+        String [] arrayNombre = {"uno", "dos", "tres"};
+        double[] arrayAltura = {1.6, 1.8};
+
+        //do - then
+        assertThrows(ArrayIndexOutOfBoundsException.class, () ->{AlturaAlumno.mostrar(arrayNombre, arrayAltura);});
+
+    }
+
+
+    @Test
+    void calculaMaximoTest1() {
+
+        //when
+        double[] arrayMax = {1.0,2.0,3.0};
+        double maximo=4.0;
 
         //do
+        double [] resultado = AlturaAlumno.calculaMaximo(arrayMax);
 
+        //then
+        assertEquals(2, resultado[0]);
+        assertEquals(3.0, resultado[1]);
+
+    }
+
+    @Test
+    void calculaMaximoTest2Vacio() {
+
+        //when
+        double[] arrayMax = new double[0];
+        double maximo=4.0;
+
+        //do
+        double [] resultado = AlturaAlumno.calculaMaximo(arrayMax);
+
+        //then
+        assertEquals(0.0, resultado[0]);
+        assertEquals(0.0, resultado[1]);
+
+    }
+
+
+    @Test
+    void calculaMaximoTest3Vacio() {
+
+        //when
+        double[] arrayMax = {0.0, -1, -2};
+
+
+        //do
+        double [] resultado = AlturaAlumno.calculaMaximo(arrayMax);
+
+        //then
+        assertEquals(0.0, resultado[0]);
+        assertEquals(0.0, resultado[1]);
+
+    }
+
+
+
+
+    @Test
+    void calculaMediaTest1() {
+
+        //when
+        double[] arrayMed = {1.0,2.0,3.0};
+        double media = 2.0;
+
+        //do
+        double resultMedia = AlturaAlumno.calculaMedia(arrayMed);
+
+        //then
+        assertEquals(2.0, resultMedia);
 
 
     }
 
 
+
+    @Test
+    void calculaMediaArrayVacioTest2() {
+
+        //when
+        double[] arrayMed = new double[0];
+
+
+        //do
+        double resultMedia = AlturaAlumno.calculaMedia(arrayMed);
+
+        //then
+        assertEquals(0.0, resultMedia);
+
+
+    }
+
+
+    @Test
+    void calculaMediaNegativoTest3() {
+
+        //when
+        double[] arrayMed = {0.0,-3.0,0.0};
+
+
+        //do
+        double resultMedia = AlturaAlumno.calculaMedia(arrayMed);
+
+        //then
+        assertEquals(-1.0, resultMedia);
+
+
+    }
 
 
 
